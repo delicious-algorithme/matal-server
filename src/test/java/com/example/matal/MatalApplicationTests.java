1c8130bc806888f8277c86de7acaf70dd991d9b5
mrri-18 <emk0528@swu.ac.kr>
2024-07-24 13:37:35 +0900

    base

---
+++ src/test/java/com/example/matal/MatalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
