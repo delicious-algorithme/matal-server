50f749add4ca3d2992833b4d8511952021200790
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2024-08-02 14:16:36 +0900

    refactor : optional 추가

--- src/main/java/com/example/matal/repository/MemberRepository.java
+++ src/main/java/com/example/matal/repository/MemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email); // 중복 가입 확인
+    Optional <Member> findByEmail(String email); // 중복 가입 확인
 }
