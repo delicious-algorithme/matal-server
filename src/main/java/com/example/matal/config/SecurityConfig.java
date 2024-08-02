88654331649b5ce1f9fe0f026a7a8bc4cf1a636a
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2024-08-02 14:06:59 +0900

    Update src/main/java/com/example/matal/config/SecurityConfig.java
    Co-authored-by: June <65225128+gogo1414@users.noreply.github.com>

--- src/main/java/com/example/matal/config/SecurityConfig.java
+++ src/main/java/com/example/matal/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
