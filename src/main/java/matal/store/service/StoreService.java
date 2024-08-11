53cd04f388788339f90addce309633e88d21a566
yeoncheong08 <yeoncheong08@gmail.com>
2024-08-11 20:42:46 +0900

    feat : 검색 기능 구현(2)

--- src/main/java/matal/store/service/StoreService.java
+++ src/main/java/matal/store/service/StoreService.java
@@ -8,8 +8,8 @@ import matal.store.repository.StoreRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -21,64 +21,27 @@ public class StoreService {
     //가게 이름 검색 조회 리스트
     public List<StoreResponseDto> StoreNameSearch(StoreRequestDto storeRequestDto) {
         List<Store> storeList = storeRepository.findByNameContaining(storeRequestDto.getName());
-        List<StoreResponseDto> storeResponseDtoList = new ArrayList<>();
 
-        for(Store store : storeList) {
-            StoreResponseDto dto = StoreResponseDto.builder()
-                    .id(store.getId())
-                    .name(store.getName())
-                    .category(store.getCategory())
-                    .reviews_count(store.getReviews_count())
-                    .address(store.getAddress())
-                    .nearby_station(store.getNearby_station())
-                    .phone(store.getPhone())
-                    .business_hours(store.getBusiness_hours())
-                    .rating(store.getRating())
-                    .build();
-            storeResponseDtoList.add(dto);
-        }
-        return storeResponseDtoList;
+        return storeList.stream()
+                .map(StoreResponseDto::new)
+                .collect(Collectors.toList());
     }
 
+    //가게 카테고리 검색 조회 리스트
     public List<StoreResponseDto> StoreCategorySearch(StoreRequestDto storeRequestDto) {
         List<Store> storeList = storeRepository.findByCategoryContaining(storeRequestDto.getCategory());
-        List<StoreResponseDto> storeResponseDtoList = new ArrayList<>();
 
-        for(Store store : storeList) {
-            StoreResponseDto dto = StoreResponseDto.builder()
-                    .id(store.getId())
-                    .name(store.getName())
-                    .category(store.getCategory())
-                    .reviews_count(store.getReviews_count())
-                    .address(store.getAddress())
-                    .nearby_station(store.getNearby_station())
-                    .phone(store.getPhone())
-                    .business_hours(store.getBusiness_hours())
-                    .rating(store.getRating())
-                    .build();
-            storeResponseDtoList.add(dto);
-        }
-        return storeResponseDtoList;
+        return storeList.stream()
+                .map(StoreResponseDto::new)
+                .collect(Collectors.toList());
     }
 
+    //가게 지하철 역 검색 조회 리스트
     public List<StoreResponseDto> StoreStationSearch(StoreRequestDto storeRequestDto) {
         List<Store> storeList = storeRepository.findByNearbyStationContaining(storeRequestDto.getNearby_station());
-        List<StoreResponseDto> storeResponseDtoList = new ArrayList<>();
 
-        for(Store store : storeList) {
-            StoreResponseDto dto = StoreResponseDto.builder()
-                    .id(store.getId())
-                    .name(store.getName())
-                    .category(store.getCategory())
-                    .reviews_count(store.getReviews_count())
-                    .address(store.getAddress())
-                    .nearby_station(store.getNearby_station())
-                    .phone(store.getPhone())
-                    .business_hours(store.getBusiness_hours())
-                    .rating(store.getRating())
-                    .build();
-            storeResponseDtoList.add(dto);
-        }
-        return storeResponseDtoList;
+        return storeList.stream()
+                .map(StoreResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
