042a5799fe141c8f8169df1e8f022e7641e8c33b
gogo1414 <wnstn6945@gmail.com>
2024-08-17 21:51:17 +0900

    refactor: StoreServiceTest 테스트 실패에 따른 일부 수정

--- src/test/java/matal/StoreServiceTest.java
+++ src/test/java/matal/StoreServiceTest.java
@@ -9,16 +9,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(properties = "spring.profiles.active=local")
-@Transactional
+@SpringBootTest
+@ActiveProfiles("local")
 public class StoreServiceTest {
+
     @Autowired
     private StoreService storeService;
 
