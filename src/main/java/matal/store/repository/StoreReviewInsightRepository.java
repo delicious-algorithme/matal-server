15069d16c98fccea1b1b393dafe584a0965e4445
yeoncheong08 <yeoncheong08@gmail.com>
2024-09-10 22:40:41 +0900

    refactor: 가독성 개선 변수 줄 분리

--- src/main/java/matal/store/repository/StoreReviewInsightRepository.java
+++ src/main/java/matal/store/repository/StoreReviewInsightRepository.java
@@ -13,39 +13,14 @@ public interface StoreReviewInsightRepository extends JpaRepository<StoreReviewI
             + "WHERE (:rating IS NULL OR sr.rating >= :rating) "
             + "AND (:positiveRatio IS NULL OR sr.positiveRatio >= :positiveRatio) "
             + "AND (:reviewword IS NULL OR sr.positiveKeywords LIKE %:reviewword% OR sr.reviewSummary LIKE %:reviewword%) "
-
             + "AND (:isSoloDining IS NULL OR sr.isSoloDining = :isSoloDining) "
             + "AND (:isParking IS NULL OR sr.isParking = :isParking)"
             + "AND (:isWaiting IS NULL OR sr.isWaiting = :isWaiting) "
             + "AND (:isPetFriendly IS NULL OR sr.isPetFriendly = :isPetFriendly)"
-
             + "ORDER BY CASE WHEN :sortBy = 'rating' THEN sr.rating "
             + "WHEN :sortBy = 'positiveRatio' THEN sr.positiveRatio "
             + "ELSE sr.rating END ASC")
     List<Long> findStoreIdsByReviewCriteriaRatingASC(@Param("rating") Double rating,
-                                            @Param("positiveRatio") Double positiveRatio,
-                                            @Param("reviewword") String reviewword,
-                                            @Param("isSoloDining") Boolean isSoloDining,
-                                            @Param("isParking") Boolean isParking,
-                                            @Param("isWaiting") Boolean isWaiting,
-                                            @Param("isPetFriendly") Boolean isPetFriendly,
-                                            @Param("sortBy") String sortBy);
-
-
-    @Query("SELECT sr.storeInfo.storeId FROM StoreReviewInsight sr "
-            + "WHERE (:rating IS NULL OR sr.rating >= :rating) "
-            + "AND (:positiveRatio IS NULL OR sr.positiveRatio >= :positiveRatio) "
-            + "AND (:reviewword IS NULL OR sr.positiveKeywords LIKE %:reviewword% OR sr.reviewSummary LIKE %:reviewword%) "
-
-            + "AND (:isSoloDining IS NULL OR sr.isSoloDining = :isSoloDining) "
-            + "AND (:isParking IS NULL OR sr.isParking = :isParking)"
-            + "AND (:isWaiting IS NULL OR sr.isWaiting = :isWaiting) "
-            + "AND (:isPetFriendly IS NULL OR sr.isPetFriendly = :isPetFriendly)"
-
-            + "ORDER BY CASE WHEN :sortBy = 'rating' THEN sr.rating "
-            + "WHEN :sortBy = 'positiveRatio' THEN sr.positiveRatio "
-            + "ELSE sr.rating END DESC")
-    List<Long> findStoreIdsByReviewCriteriaRatingDESC(@Param("rating") Double rating,
                                                      @Param("positiveRatio") Double positiveRatio,
                                                      @Param("reviewword") String reviewword,
                                                      @Param("isSoloDining") Boolean isSoloDining,
@@ -53,4 +28,25 @@ public interface StoreReviewInsightRepository extends JpaRepository<StoreReviewI
                                                      @Param("isWaiting") Boolean isWaiting,
                                                      @Param("isPetFriendly") Boolean isPetFriendly,
                                                      @Param("sortBy") String sortBy);
+
+
+    @Query("SELECT sr.storeInfo.storeId FROM StoreReviewInsight sr "
+            + "WHERE (:rating IS NULL OR sr.rating >= :rating) "
+            + "AND (:positiveRatio IS NULL OR sr.positiveRatio >= :positiveRatio) "
+            + "AND (:reviewword IS NULL OR sr.positiveKeywords LIKE %:reviewword% OR sr.reviewSummary LIKE %:reviewword%) "
+            + "AND (:isSoloDining IS NULL OR sr.isSoloDining = :isSoloDining) "
+            + "AND (:isParking IS NULL OR sr.isParking = :isParking)"
+            + "AND (:isWaiting IS NULL OR sr.isWaiting = :isWaiting) "
+            + "AND (:isPetFriendly IS NULL OR sr.isPetFriendly = :isPetFriendly)"
+            + "ORDER BY CASE WHEN :sortBy = 'rating' THEN sr.rating "
+            + "WHEN :sortBy = 'positiveRatio' THEN sr.positiveRatio "
+            + "ELSE sr.rating END DESC")
+    List<Long> findStoreIdsByReviewCriteriaRatingDESC(@Param("rating") Double rating,
+                                                      @Param("positiveRatio") Double positiveRatio,
+                                                      @Param("reviewword") String reviewword,
+                                                      @Param("isSoloDining") Boolean isSoloDining,
+                                                      @Param("isParking") Boolean isParking,
+                                                      @Param("isWaiting") Boolean isWaiting,
+                                                      @Param("isPetFriendly") Boolean isPetFriendly,
+                                                      @Param("sortBy") String sortBy);
 }
