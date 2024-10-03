cfe35ac3fe188e97c98a509b130bcf7b3eca94a5
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2024-10-03 16:36:37 +0900

    Update MemberRepository.java

--- src/main/java/matal/member/repository/MemberRepository.java
+++ src/main/java/matal/member/repository/MemberRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
-    boolean existsByEmail(String email);// 중복 가입 확인
+
 }
