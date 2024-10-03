f8c498ca1715b5cbe99e296c250e1718147d10f8
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2024-10-03 14:02:27 +0900

    Update src/main/java/matal/member/service/MemberService.java
    Co-authored-by: June <65225128+gogo1414@users.noreply.github.com>

--- src/main/java/matal/member/service/MemberService.java
+++ src/main/java/matal/member/service/MemberService.java
@@ -6,10 +6,10 @@ import matal.member.dto.MemberSignUpRequestDto;
 
 
 public interface MemberService {
+
     public LoginResponse login(MemberLoginDto requestDto) throws Exception;
 
     public Long signUp(MemberSignUpRequestDto requestDto) throws Exception;
 
     public void logout(String token) throws Exception;
-
 }
