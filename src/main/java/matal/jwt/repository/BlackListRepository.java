e05b2d77c3aa4015f1749c214f0bc441438012ef
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2024-10-03 13:29:18 +0900

    Update src/main/java/matal/jwt/repository/BlackListRepository.java
    Co-authored-by: June <65225128+gogo1414@users.noreply.github.com>

--- src/main/java/matal/jwt/repository/BlackListRepository.java
+++ src/main/java/matal/jwt/repository/BlackListRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlackListRepository extends JpaRepository<BlackList, Long> {
+
     boolean existsByInvalidRefreshToken(String token);
 }
