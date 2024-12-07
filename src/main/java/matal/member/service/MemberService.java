f55da697d3f12a001f0f285d47535a287edc0a39
gogo1414 <wnstn6945@gmail.com>
2024-12-07 22:00:13 +0900

    refactor: 회원정보 저장 시 약관 정보 추가

--- src/main/java/matal/member/service/MemberService.java
+++ src/main/java/matal/member/service/MemberService.java
@@ -69,6 +69,9 @@ public class MemberService {
                 .password(passwordEncoder.encode(signUpRequestDto.password()))
                 .nickname(signUpRequestDto.nickname())
                 .role(Role.MEMBER)
+                .privacyAgreement(signUpRequestDto.privacyAgreement())
+                .serviceAgreement(signUpRequestDto.serviceAgreement())
+                .ageConfirmation(signUpRequestDto.ageConfirmation())
                 .build();
 
         memberRepository.save(newMember);
